28308e1e650b5ed195d8bac1c7f9ff4eb0f5c077
yo_yamamoto <ebsw0820e@gmail.com>
2019-09-02 12:17:18 +0900

    イベントとかステートとかバインドとか色々

--- src/js/react.jsx
+++ src/js/react.jsx
@@ -1,41 +1,59 @@
 /*
- * other
-const MySecClass = React.cleateClass({
-  render:function(){
-    return (
-      <h2 className="sec">{this.props.message}</h2>
-    )
-  }
-});
-*/
-
 class Head extends React.Component {
   render(){ return (
-    <h1>{this.props.message}</h1>
+    <h1 className="Head">{this.props.message}</h1>
   )}
-}
+}*/
 
 class Form extends React.Component{
-  render(){ return (
-    <div>
-        <input type="text"/>
-        <button>{this.props.label}</button>
-    </div>
-  )}
+
+  constructor(props){
+    super(props);
+    this.state = {
+      value : '',
+      message : ''
+    }
+    this.inputOnChangeFunc = this.inputOnChangeFunc.bind( this );
+    this.buttonOnClickFunc = this.buttonOnClickFunc.bind( this );
+  }
+
+  inputOnChangeFunc ({ target : { value } }) {
+    console.log( "onChange" + value);
+    this.setState({
+      value
+    });
+  }
+
+  buttonOnClickFunc () {
+    console.log( "send" + this.state.value );
+    const { value } = this.state;
+    console.log( this.state );
+    this.setState({
+      value : '',
+      message : value
+    })
+  }
+  
+  render () {
+    return (
+      <div className="Form">
+          <input type="text" value={ this.state.value } onChange={ this.inputOnChangeFunc } />
+          <button onClick={ this.buttonOnClickFunc }>{ this.props.label }</button>
+          <div>{ this.state.message }</div>
+      </div>
+    );
+  }
 }
 
 class Container extends React.Component{
   render(){ return (
-    <Head message="REACT sample" />
-    
-  )}
+    //<Head message="REACT sample" />
+    <Form label="送信" />
+  );}
 }
 
 ReactDOM.render(
   <Container />,
   document.getElementById('root')
 );
-
-
-
 
